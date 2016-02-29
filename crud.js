09f5d8557c8092df201bad4c9b8f06cb0b07cf21
topleft <pete.james.jeffryes@gmail.com>
2016-02-29 07:30:47 -0700

    edit toggle working

--- crud.js
+++ crud.js
@@ -1,13 +1,16 @@
-Items = new Mongo.Collection("items")
+Items = new Mongo.Collection("items");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('toggle', false);
 
   Template.body.helpers({
     items: function () {
       return Items.find({});
+    },
+    toggle: function () {
+      return Session.get("toggle");
     }
+
   });
 
   Template.body.events({
@@ -19,9 +22,24 @@ if (Meteor.isClient) {
       Items.insert({name: item, type: type});
       event.target.item.value = "";
       event.target.type.value = "";
-      
+
+    },
+    'click #edit': function () {
+      Session.set('toggle', this.item._id);
+    },
+    'click #delete': function () {
+      Items.remove(this.item._id);
     }
   });
+
+  Template.row.helpers({
+    editToggle: function () {
+      return 
+    },     
+    matching: function (a, b) {
+      return a === b;
+    }
+  })
 }
 
 if (Meteor.isServer) {
